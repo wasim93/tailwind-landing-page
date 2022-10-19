039cde574ba521ae16aede779fafda81ba1363ea
Wasim Akram <muhammad.wasim93@gmail.com>
2022-10-19 13:05:13 +0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(204, 70%, 53%)',
+        brightRedLight: 'hsl(204, 70%, 63%)',
+        brightRedSupLight: 'hsl(204, 70%, 81%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+    },
+  },
+  plugins: [],
+};
